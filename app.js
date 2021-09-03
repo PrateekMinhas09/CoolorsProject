2c7147a5532c1f2e6ba29621dc27ceedb70f0ac6
OmmziDeveloper <58804516+OmmziDeveloper@users.noreply.github.com>
2021-09-03 23:40:15 +0530

    created a color generator

---
+++ app.js
@@ -0,0 +1,44 @@
+
+//global selections and variables 
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(`.generate`);
+const sliders = document.querySelectorAll(`input[type="range"]`);
+const currentHexes = document.querySelectorAll(`.color h2`);
+let initialColors;
+
+
+//functions 
+//color generator
+function generateHex()
+{
+    /*const letters = `0123456789ABCDEF`;
+    let hash = `#`;
+    for(i=0;i<6;i++)
+    {
+        hash+=letters[Math.floor(Math.random()*16)];
+    }
+    return hash;*/
+
+    //using chrmoa js we can just  do 
+    const hexcolor = chroma.random();
+    console.log(hexcolor);
+    return hexcolor;
+    
+}
+
+
+function randomColors()
+{
+colorDivs.forEach((div,index)=>{
+
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+
+
+});
+}
+randomColors();
